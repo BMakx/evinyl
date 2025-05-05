1c8346e32b17e592d111904791b241f358cc2d8f
BMakx <135823853+BMakx@users.noreply.github.com>
2025-05-05 14:05:55 +0300

    Update settings.h

--- E-vinyl/esp32SpotifyEchoDot/settings.h
+++ E-vinyl/esp32SpotifyEchoDot/settings.h
@@ -1,9 +1,9 @@
 // WiFi settings
-const char* ssid = "sqrt(5,76)GHz";
-const char* pass = "AhaMelina23!";
+const char* ssid = "your ssid";
+const char* pass = "your password";
 
 // Spotify settings
-String deviceName = "Bajtek";
-String clientId = "df744a36e1d14361a7c7998e24df2b47";
-String clientSecret = "e6bb529bb2ee48858f021713de8c9020";
-String refreshToken = "AQBGAYjikzwowFPeL_Zv1S3Q2tMm7mPc0FPI1-N7BxpQEgCdbca0pOxU7klpIir6fp7Eu0KTU5Sgcj-X0NBNUJJgf41Fd3-bLq1GF56AX_wj_1gFSDHkexLkPid1GJplD94";
+String deviceName = "deviceName";
+String clientId = " clientId";
+String clientSecret = "clientSecre";
+String refreshToken = "refreshToken";
